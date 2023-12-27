184e51d0974a29509eb1b5840214b7868e81e391
brachi-f <braf5866@gmail.com>
2023-12-27 02:26:47 +0200

    27-12-2023 02:26

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './recipes/updateRec';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App IdRecipe={1}/>
   </React.StrictMode>
 );
 
